e503feb10c516310edbc598386a5381960a4fd81
Braun Goodson <braun.goodson@digitaslbi.com>
2017-08-08 21:39:00 -0400

    added

---
+++ devops/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this file aquires binary dependencies for the build run-time
+
+# update this machine
+sudo yum update -y
+
+# add git
+sudo yum install -y git
+
+# should get zsh here
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+# source it
+. ~/.nvm/nvm.sh
+
+# install nodejs
+nvm install v8.2.1
+
+# install yarn
+npm install -g yarn
+
+# add enterprise software development tools
+yarn global add vue-cli
+
+# then just do your stuff, like `vue init webpack`
+
+# `yarn add global serve`
---
+++ devops/prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# check the routes
+sudo iptables -t nat -L
+
+# reroute incoming packets on 80 to 8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
+
+# get production server
+yarn add global serve
+
+# serve current directory (e.g., `cd dist/ && serve`)
+serve --port 8000
